350e74a5efe502a129f4e7ed09420492f31f780b
hGriff0n <ghooper96@gmail.com>
2016-07-04 19:25:00 -0400

    Added tailrec annotation in utils

--- caeser.scala
+++ caeser.scala
@@ -2,6 +2,9 @@ package crypto.classical
 
 import crypto.Cipher;
 
+// If i can define a "CipherApply" trait/ADT, I can move encode to Cipher and
+    // implement encrypt and decrypt in the Cipher trait (encode would take a CipherApply)
+    // should wait until i get to more complex encryption schemes til I decide
 class Caeser(val shft: Int) extends Cipher {
     private def encode(c: Char, sh: Int): Char = c match {
         case c if lc.contains(c) =>
--- utils.scala
+++ utils.scala
@@ -1,27 +1,29 @@
 package crypto
 
+import scala.annotation.tailrec;
+
 // Package object allows "free functions" to be used int library by importing crypto.utils._
 package object utils {
     // Perform the extended euclidean algorithm to find gcd(a, b)
-        // Returns a tuple (min(a, b), inverse, coprime)
+        // Returns a tuple (x=min(a, b), x^-1(mod 26), coprime(a, b))
     def euclid(a: Int, b: Int): (Int, Int, Boolean) = {
-        //@tailrec
+        @tailrec
         def aux(a: Int, b: Int, x: Int, y: Int, xl: Int, yl: Int): (Int, Int) = {
             if (a == 0) return (xl, yl)
             val (q, r) = (b / a, b % a)
             aux(r, a, xl - q * x, yl - q * y, x, y)
         }
 
-        // The variables' names assume that a < b
+        // The names assume that a < b
         val (inv, co) = aux(a, b, 1, 0, 0, 1)
-        if (a > b) return (b, co, inv == 1)
-        (a, inv, co == 1)
+        if (a > b) (b, co, inv == 1)
+        else       (a, inv, co == 1)
     }
     
     def modInv(a: Int, m: Int): Option[Int] = {
         val (_, inv, coprime) = euclid(a, m)
-        if (coprime) return Some((inv + m) % m)
-        None
+        if (coprime) Some((inv + m) % m)
+        else         None
     }
  
 }
