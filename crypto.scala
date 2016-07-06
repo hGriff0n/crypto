465bfd8ef1cba79f8cb2be197badb6da280e49a7
hGriff0n <ghooper96@gmail.com>
2016-07-06 17:44:59 -0400

    Added Identity Cipher

--- crypto.scala
+++ crypto.scala
@@ -6,8 +6,8 @@ import utils._;
 
 object demo {
     def main(args: Array[String]): Unit = {        
-        val cipher = new Rot13
-        val msg = cipher.encrypt("Why did the chicken cross the road?\nGb trg gb gur bgure fvqr!")
+        val cipher = new Iden
+        val msg = cipher.encrypt("Hello")
 
         println(msg)    // irk
         println(cipher.decrypt(msg)) // rip
---
+++ iden.scala
@@ -0,0 +1,6 @@
+package crypto;
+
+class Iden extends Cipher {
+    def encrypt(msg: String) = msg
+    def decrypt(msg: String) = msg
+}
