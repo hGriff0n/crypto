66a73e01ff76b409e7a420b4eb1b2fdf22c6c71d
hGriff0n <ghooper96@gmail.com>
2016-07-12 13:11:26 -0400

    Converted Caeser to Use utils.tabula

--- src/main/scala/caeser.scala
+++ src/main/scala/caeser.scala
@@ -1,21 +1,17 @@
 package crypto.classical
 
 import crypto.Cipher;
+import crypto.utils.tabula;
 
 // If i can define a "CipherApply" trait/ADT, I can move encode to Cipher and
     // implement encrypt and decrypt in the Cipher trait (encode would take a CipherApply)
     // should wait until i get to more complex encryption schemes til I decide
 class Caeser(val shft: Int) extends Cipher {
-    private def encode(c: Char, sh: Int): Char = c match {
-        case c if lc.contains(c) =>
-        	((c.toInt - 71 + sh) % 26 + 97).toChar
-        case c if uc.contains(c) =>
-        	((c.toInt - 39 + sh) % 26 + 65).toChar
-        case c => c
-    }
+    def encrypt(msg: String): String =
+        for (c <- msg.toUpperCase) yield tabula(c, shft)
 
-    def encrypt(msg: String): String = for (c <- msg) yield encode(c, shft)
-    def decrypt(msg: String): String = for (c <- msg) yield encode(c, -shft)
+    def decrypt(msg: String): String =
+        for (c <- msg) yield tabula(c, -shft)
 }
 
 class Rot13 extends Caeser(13)
--- src/main/scala/crypto.scala
+++ src/main/scala/crypto.scala
@@ -6,8 +6,8 @@ import utils._;
 
 object demo {
     def main(args: Array[String]): Unit = {        
-        val cipher = new ADFGVX("PRIVACY")
-        val msg = cipher.encrypt("ATTACK at 1200am")
+        val cipher = new Rot13
+        val msg = cipher.encrypt("IRQ")
 
         // VIC: ATTACK AT DAWN => ANWHRSANROAEER
         // Trifid: Treaty ends Boer War. => MUAFN.EQRKREUTXQBW
